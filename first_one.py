58fca8c0e53402b17685312da391e2143b60947d
hellvillain <ofchildbirth@gmail.com>
2016-04-25 23:34:50 +0300

    Update first_one.py

--- first_one.py
+++ first_one.py
@@ -1,42 +1,41 @@
-import itertools
-import os
+import tempfile
 
-alist = range(1, 20, 1)
+alist_1 = range(1, 20, 1)
 alist_2 = range(1, 20, 1)
 alist_3 = range(1, 20, 3)
 
 
-# a. making squares from a list of numbers
-def squaring (list):
-    alist = [x**2 for x in list]
-    return alist
+# a. 5a: making squares from a list of numbers
+def squaring(alist):
+    return [x**2 for x in alist]
+
+# a. 5a: with lambda
+lambda_squaring = lambda alist: [x**2 for x in alist]
+
+# b. 5b: making squares for each second from a list of numbers
+def each_second(alist):
+    return [x for x in alist if x % 2 == 0]
 
 
-# b. making squares for each second from a list of numbers
-def each_second (list):
-    alist = [x for x in list if x%2 != 0]
-    return alist
+# c. 5c: squares of even elements on uneven positions;
+def mixed_second(alist):
+    return [x**2 for x in alist if x % 2 == 0 and alist.index(x) % 2 != 0]
 
 
-# c. squares of even elements on uneven positions;
-def mixed_second (list):
-    alist = [x**2 for x in list if x%2 == 0 and list.index(x)%3 == 0]
-    return alist
-
-
-# d. own zip perfomance
+# d. task 4b: own zip perfomance
 def zipping(alist_1, alist_2):
     if len(alist_1) != len(alist_2):
         print "You brought up lists with wrong length"
+        return None
     else:
-        zipped = [x*y for x in alist_1 for y in alist_2 if alist_1.index(x)==alist_2.index(y)]
-        return zipped
+        zipped = ((x,y) for x in alist_1 for y in alist_2 if alist_1.index(x) == alist_2.index(y))
+        return list(zipped)
 
 
 # f. creating cat function
-filepath_one = "C:/Users/ashapkin/Documents/Training/sin.txt"
-filepath_two = "C:/Users/ashapkin/Documents/Training/bread.txt"
-final_destiny = "C:/Users/ashapkin/Documents/Training/vasiliy.txt"
+filepath_one = "C:/Users/Alexis/Documents/Training/Stager/sin.txt"
+filepath_two = "C:/Users/Alexis/Documents/Training/Stager/bread.txt"
+final_destiny = "C:/Users/Alexis/Documents/Training/Stager/vasiliy.txt"
 
 
 def cat(*args):
@@ -48,11 +47,8 @@ def cat(*args):
 
 cat(filepath_one, filepath_two)
 
-b = [4 for x in range(1,10,2)]
-
-print b
-print squaring(alist)
-print each_second(alist)
-print mixed_second(alist)
-print my_chain(alist, alist_2)
-print zipping(alist, alist_2)
+print zipping(alist_1, alist_2)
+print zip(alist_1, alist_2)
+print each_second(alist_2)
+print "mixed:", list(mixed_second(alist_2))
+print "lambda_squaring", lambda_squaring(alist_2)
