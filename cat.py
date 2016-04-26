8c27182213a2f0b717ec56a124a1ad57adccac11
hellvillain <ofchildbirth@gmail.com>
2016-04-26 17:38:13 +0300

    Create cat.py

---
+++ cat.py
@@ -0,0 +1,15 @@
+# 6. creating cat function
+# Am I supposed to create a file and fill it up with data on-the-go?
+# filepath_one = "C:/Users/Alexis/Documents/Training/Stager/sin.txt"
+# filepath_two = "C:/Users/Alexis/Documents/Training/Stager/bread.txt"
+# final_destiny = "C:/Users/Alexis/Documents/Training/Stager/vasiliy.txt"
+
+
+# def cat(*args):
+#     with tempfile.TemporaryFile() as tmp:
+#         for every in args:
+#             tmp.writelines(line if '\n' in line else "%s\n" % line for line in open(every, 'r'))
+#         tmp.seek(0)
+#         print tmp.read()
+
+# cat(filepath_one, filepath_two)
