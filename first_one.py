e95d85126d8c42106345cb4c0a83861021455bbe
hellvillain <ofchildbirth@gmail.com>
2016-04-26 17:21:51 +0300

    Update first_one.py

--- first_one.py
+++ first_one.py
@@ -19,12 +19,13 @@ def each_second(alist):
 # 5b. 2, lambda ver.: each second element from a list of numbers
 lambda_each_second = lambda alist: [x for x in alist if x % 2 == 0]
 
-# c. 5c: squares of even elements on uneven positions;
+# 5c: squares of even elements on uneven positions;
+# Is it possible to avoid double "for statement" for this task and write it in one\two lines?
 def mixed_second(alist):
     return [x**2 for x in alist if x % 2 == 0 and alist.index(x) % 2 != 0]
 
 
-# d. task 4b: own zip perfomance
+# 4b. task 4b: own zip perfomance
 def zipping(alist_1, alist_2):
     if len(alist_1) != len(alist_2):
         print "You brought up lists with wrong length"
@@ -34,24 +35,25 @@ def zipping(alist_1, alist_2):
         return list(zipped)
 
 
-# f. creating cat function
+# 6. creating cat function
+# Am I supposed to create a file and fill it up with data on-the-go? 
 # filepath_one = "C:/Users/Alexis/Documents/Training/Stager/sin.txt"
 # filepath_two = "C:/Users/Alexis/Documents/Training/Stager/bread.txt"
 # final_destiny = "C:/Users/Alexis/Documents/Training/Stager/vasiliy.txt"
 
 
-def cat(*args):
-    with tempfile.TemporaryFile() as tmp:
-        for every in args:
-            tmp.writelines(line if '\n' in line else "%s\n" % line for line in open(every, 'r'))
-        tmp.seek(0)
-        print tmp.read()
+# def cat(*args):
+#     with tempfile.TemporaryFile() as tmp:
+#         for every in args:
+#             tmp.writelines(line if '\n' in line else "%s\n" % line for line in open(every, 'r'))
+#         tmp.seek(0)
+#         print tmp.read()
 
 # cat(filepath_one, filepath_two)
 
 print "my zip output:", zipping(alist_1, alist_2)
 print "original zip output:", zip(alist_1, alist_2)
 print "each_second:", each_second(alist_2)
-print "mixed:", list(mixed_second(alist_2))
+# print "mixed:", list(mixed_second(alist_2))
 print "lambda_squaring:", lambda_squaring(alist_2)
 print "lambda_each_second:", lambda_each_second(alist_2)
