c2b8fa11aafeb3a56d9c28d8c7463288283b77d6
hellvillain <ofchildbirth@gmail.com>
2016-05-19 18:05:46 +0300

    Update list_comprehension.py

--- list_comprehension.py
+++ list_comprehension.py
@@ -1,5 +1,5 @@
-alist_1 = range(1, 20, 1)
-alist_2 = range(1, 20, 1)
+alist_1 = range(1, 20)
+alist_2 = range(0, 20)
 
 
 # 5a, 1, def ver.: making squares from a list of numbers
@@ -9,24 +9,29 @@ def squaring(alist):
 # 5a, 2, lambda ver.:  making squares from a list of numbers
 lambda_squaring = lambda alist: [x**2 for x in alist]
 
+
 # 5b, 1, def ver.: each second element from a list of numbers
 def each_second(alist):
     return [x for x in alist if x % 2 == 0]
 
+
+# 5b, 1, def ver.: right version with slice
+def each_second_slice(alist):
+    return alist[0::2]
+
+
 # 5b, 2, lambda ver.: each second element from a list of numbers
 lambda_each_second = lambda alist: [x for x in alist if x % 2 == 0]
 
-# 5c: squares of even elements on uneven positions;
-# Is it possible to avoid double "for statement" for this task and write it in one\two lines?
-def mixed_second(alist):
-    return [x**2 for x in alist if x % 2 == 0 and alist.index(x) % 2 != 0]
 
-def each_second_mixed(alist):
-    return [x*x for x in alist if x % 2 == 0][::3]
+# 5c: each_second_mixed starting with zero, slice version 
+def each_second_mixed_slice(alist):
+    return [x*x for x in alist if x % 2 == 0][1::2]
 
 
 print "5a, 1: squaring:", squaring(alist_2)
 print "5a, 2: lambda_squaring:", lambda_squaring(alist_2)
-print "5b, 1: each_second:", each_second(alist_2)
+print "5b, 1: each_second, bycicle type:", each_second(alist_2)
+print "5b, 1: each_second with slices:", each_second_slice(alist_2)
 print "5b, 2: lambda_each_second:", lambda_each_second(alist_2)
-# print "mixed:", list(mixed_second(alist_2))
+print "5c: mixed with slices:", each_second_mixed_slice(alist_2)
