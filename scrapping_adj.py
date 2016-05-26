5abe44f412a750abc8494e46e9810b211c4022be
hellvillain <ofchildbirth@gmail.com>
2016-05-27 00:40:50 +0300

    Update scrapping_adj.py

--- scrapping_adj.py
+++ scrapping_adj.py
@@ -7,14 +7,30 @@ import requests
 # resp_one = requests.get(domain, stream=True)
 # b = resp_one.text
 # print b
+options = {'Departure': 'PEK',
+           'Destination': 'LED',
+           'Outbound': '2016-06-16',
+           "ReturnDate": ""}
 
-def to_start(Departure, Destination, OutboundDate, ReturnDate=""):
-    domain = "http://www.flyniki.com/en/booking/flight/vacancy.php?departure=CEK&destination=PEK&outboundDate=2016-06-16&returnDate=&oneway=1&openDateOverview=0&adultCount=1&childCount=0&infantCount=0"
+
+def to_start(kwargs):
+    if(len(kwargs["ReturnDate"])>0):
+        domain = "http://www.flyniki.com/en/booking/flight/vacancy.php?departure={Departure}" \
+             "&destination={Destination}" \
+             "&outboundDate={Outbound}" \
+             "&returnDate={ReturnDate}&oneway=0" \
+             "&openDateOverview=0&adultCount=1&childCount=0&infantCount=0".format(**kwargs)
+    elif(len(kwargs["ReturnDate"])==0):
+        domain = "http://www.flyniki.com/en/booking/flight/vacancy.php?departure={Departure}" \
+             "&destination={Destination}" \
+             "&outboundDate={Outbound}" \
+             "&returnDate={ReturnDate}&oneway=1" \
+             "&openDateOverview=0&adultCount=1&childCount=0&infantCount=0".format(**kwargs)
     resp_one = requests.get(domain, stream=True)
     ssid_url = resp_one.url
     return resp_one.url
 
 
-print to_start("PEK", "DME", "2016-06-16", "2016-06-24")
+print to_start(options)
 
 # Location: http://www.flyniki.com/en/booking/flight/vacancy.php?departure=CEK&destination=PEK&outboundDate=2016-06-16&returnDate=&oneway=1&openDateOverview=0&adultCount=1&childCount=0&infantCount=0
